106ca6d521982a4b85c6ce1ca22f62197a231e71
MaikelGevdLeeuw <maikelgevdleeuw@outlook.com>
2023-04-21 13:12:57 +0200

    Uitwerkingen

---
+++ dist/index.816e7b21.js
@@ -0,0 +1,6 @@
+console.log("Hallo daar!");
+async function fetchCountries() {
+    const response = await axios.get();
+}
+
+//# sourceMappingURL=index.816e7b21.js.map
--- src/app.js
+++ src/app.js
@@ -1,1 +1,38 @@
+import axios from "axios";
+
 console.log('Hallo daar!');
+
+
+
+const countryList = document.getElementById("countries");
+const errorMessage = document.getElementById("error")
+
+async function fetchCountries(){
+    try {
+        const response = await axios.get("https://restcountries.com/v3.1/all?fields=name,population,flags,region");
+        const countries = response.data.map(country => ({
+            name: country.name.common,
+            flag: country.flags.svg,
+            population: country.population,
+            region: country.region
+        }));
+        return countries;
+    } catch  (e) {
+        console.error(e);
+        return null;
+    }
+}
+
+
+fetchCountries().then((countries) => {
+    const sortedCountries = countries.sort((a, b) => a.population - b.population);
+    for (let i = 0; i < sortedCountries.length ; i++){
+        let regionClass = sortedCountries[i].region.toLowerCase().replace(" ", "-");
+        countryList.innerHTML += ` <li class="${regionClass}"> <img src="${sortedCountries[i].flag}"> <span class="country-name">${sortedCountries[i].name}</span> <br> Has a population of ${sortedCountries[i].population} people</li>`;
+    }
+});
+
+
+
+
+
